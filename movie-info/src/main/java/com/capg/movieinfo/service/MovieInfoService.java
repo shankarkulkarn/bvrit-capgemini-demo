ff1359b5795d85547a0509eea23729b70969dd9f
shankar <shankarkulkarn@gmail.com>
2020-04-10 10:50:35 +0530

    commit

---
+++ movie-info/src/main/java/com/capg/movieinfo/service/MovieInfoService.java
@@ -0,0 +1,32 @@
+package com.capg.movieinfo.service;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.capg.movieinfo.dao.MovieInfoDao;
+import com.capg.movieinfo.exception.MovieException;
+import com.capg.movieinfo.model.MovieInfo;
+
+@Service
+public class MovieInfoService {
+	
+	//List<MovieInfo> movies=Arrays.asList(new MovieInfo(1001,"Gita Govindam","Love Stroy"),
+			//new MovieInfo(1002,"Bahubali","Drama")
+			//);
+	
+	@Autowired
+	MovieInfoDao  movieInfoDao;
+
+	public MovieInfo getMovieById(int id) throws MovieException {
+		//return movies.stream().filter(m->m.getMovieId()==id).findFirst().orElse(null);
+		if(!movieInfoDao.existsById(id))
+		{
+			
+			 throw new MovieException(" ID NOT FOUND");
+		}
+		return movieInfoDao.findById(id).get();
+	}
+}
